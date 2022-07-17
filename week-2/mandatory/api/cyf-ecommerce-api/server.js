5ceb9031e849abb96a00b88849530eb6974f7a3a
GloriaKwofie <91749390+GloriaKwofie@users.noreply.github.com>
2022-07-17 11:40:29 +0200

    week three database homework done

--- week-2/mandatory/api/cyf-ecommerce-api/server.js
+++ week-2/mandatory/api/cyf-ecommerce-api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { Pool, Client } = require("pg");
 const bodyParser = require("body-parser");
+//const { request } = require("express");
 
 const PORT = 4000;
 const app = express();
@@ -9,11 +10,13 @@ const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "cyf_commerce",
-  password: "Generations39",
+  password: "",
   port: 5432,
 });
 
 app.use(bodyParser.json());
+
+// Week two Homework
 
 app.get("/customers", function (req, res) {
   pool.query("SELECT * FROM customers", (response, error) => {
@@ -22,6 +25,13 @@ app.get("/customers", function (req, res) {
     }
     res.json(response.rows);
   });
+});
+
+app.get("/customers", (req, res) => {
+  pool
+    .query("select * from customers")
+    .then((response) => res.json(response.rows))
+    .catch((error) => console.log("Something went wrong " + error));
 });
 
 app.get("/suppliers", function (req, res) {
@@ -46,6 +56,19 @@ app.get("/products", function (req, res) {
       res.json(response.rows);
     }
   );
+});
+
+//Week three homework
+
+app.get("/products", async (req, res) => {
+  try {
+    const selectProducts = await pool.query(
+      "select products.product_name,suppliers.supplier_name from products INNER join suppliers on products.supplier_id=suppliers.id"
+    );
+    res.json(selectProducts.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 app.get("/products", (req, res) => {
@@ -76,6 +99,22 @@ app.get("/products", (req, res) => {
 });
 //Add a new GET endpoint /customers/:customerId to load a single customer by ID.
 
+app.get("/customers/:customerId", (req, res) => {
+  const customerId = req.params.customerId;
+
+  const customersById = "select * from customers where id = $1";
+
+  pool
+    .query(customersById, [customerId])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        res.json(result.rows);
+      } else {
+        res.status(400).send(`ID does not exist`);
+      }
+    })
+    .catch((error) => console.log("Something is wrong " + error));
+});
 //Add a new POST endpoint /customers to create a new customer.
 
 app.post("/customers", function (req, res) {
@@ -114,7 +153,7 @@ app.post("/products", function (req, res) {
           "INSERT INTO products (product_name, unit_price, supplier_id) VALUES ($1, $2, $3)";
         pool
           .query(query, [productName, price, supplierId])
-          .then(() => res.send("Hotel created!"))
+          .then(() => res.send("New product created!"))
           .catch((e) => console.error(e));
       }
     });
@@ -123,7 +162,6 @@ app.post("/products", function (req, res) {
 //Add a new POST endpoint /customers/:customerId/orders to create a new order (including an order date, and an order reference) for a customer. Check that the customerId corresponds to an existing customer or return an error.
 app.post("/customers/:customerId/orders", (req, res) => {
   let customerId = req.params.customerId;
-
   let orderDate = req.body.order_date;
   let orderRef = req.body.order_reference;
   console.log("Id " + customerId);
@@ -149,7 +187,82 @@ app.post("/customers/:customerId/orders", (req, res) => {
     .catch((error) => console.error("Something is wrong " + error));
 });
 
-//Add a new POST endpoint /customers/:customerId/orders to create a new order (including an order date, and an order reference) for a customer. Check that the customerId corresponds to an existing customer or return an error
+//Add a new PUT endpoint /customers/:customerId to update an existing customer (name, address, city and country).
+
+app.put("/customers/:customerId", function (req, res) {
+  const name = req.params.name;
+  const address = req.body.address;
+  const city = req.body.city;
+  const country = req.body.country;
+
+  const queryResult =
+    "UPDATE customers SET  name=$1, address=$2, city=$3, country=$4 WHERE id=$5";
+  pool
+    .query(queryResult, [name, address, city, country, customerId])
+    .then(() => res.send(`Customer ${customerId} updated!`))
+    .catch((e) => console.error(e));
+});
+
+//Add a new DELETE endpoint /orders/:orderId to delete an existing order along all the associated order items.
+
+app.delete("/orders/:orderId", function (req, res) {
+  const orderId = req.params.orderId;
+
+  pool
+    .query("DELETE FROM order_items WHERE order_id=$1", [orderId])
+    .then(() => {
+      pool
+        .query("DELETE FROM order WHERE id=$1", [orderId])
+        .then(() => res.send(`order ${orderId} has been deleted!`))
+        .catch((e) => console.error(e));
+    })
+    .catch((e) => console.error(e));
+});
+
+//Add a new DELETE endpoint /customers/:customerId to delete an existing customer only if this customer doesn't have orders.
+
+app.delete("/customers/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+  const checkOrdersOfTheCustomer =
+    "select * from orders where orders.customer_id = $1";
+  const deleteClient = "DELETE from customers where id = $1";
+
+  pool
+    .query(checkOrdersOfTheCustomer, [customerId])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        res
+          .status(400)
+          .send(
+            " Customer can not be deleted " +
+              customerId +
+              " because it has orders"
+          );
+      } else {
+        pool
+          .query(deleteClient, [customerId])
+          .then(() => res.send(`Customer ${customerId} deleted!`))
+          .catch((e) => console.error(e));
+      }
+    })
+    .catch((error) => console.error("Something is wrong " + error));
+});
+// Add a new GET endpoint /customers/:customerId/orders to load all the orders along the items in the orders of a specific customer.
+// Especially, the following information should be returned: order references, order dates, product names, unit prices, suppliers and quantities.
+app.get("/customers/:customerId/orders", (req, res) => {
+  let customerId = req.params.customerId;
+
+  const getCustomerOrders =
+    "select o.order_reference, o.order_date, p.product_name, p.unit_price, s.supplier_name " +
+    "from orders o join order_items oi on o.id = oi.order_id " +
+    "join products p on p.id = oi.product_id " +
+    "join suppliers s on p.supplier_id = s.id " +
+    "where o.customer_id = $1";
+  pool
+    .query(getCustomerOrders, [customerId])
+    .then((result) => res.json(result.rows))
+    .catch((error) => console.error("Something is wrong " + error));
+});
 
 app.listen(PORT, function () {
   console.log(
